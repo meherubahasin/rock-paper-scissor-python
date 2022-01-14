f5a12ebaa492506d4aef8947b23e6a7e88ca4038
Meheruba Hasin <87016852+meherubahasin@users.noreply.github.com>
2022-01-14 13:02:59 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,71 @@
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+#Write your code below this line 👇
+import random
+
+gameimages = [rock, paper, scissors]
+
+userchoice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+
+computerchoice = random.randint(0, 2)
+
+print("\n")
+
+print("You chose:\n")
+if userchoice >= 3 or userchoice < 0:
+  print("Invalid number!")
+else:
+  print(gameimages[userchoice])
+
+print("\n")
+
+print(f"Computer chose:\n")
+print(gameimages[computerchoice])
+
+
+
+if userchoice == computerchoice:
+  print("Draw.")
+
+#losing conditions
+elif userchoice == 1 and computerchoice == 2:
+  print("You lose.")
+elif userchoice == 0 and computerchoice == 1:
+  print("You lose.")
+elif userchoice == 2 and computerchoice == 0:
+  print("You lose.")
+elif userchoice >= 3 or userchoice < 0:
+  print("You lose.")
+
+#wining conditions
+elif userchoice == 2 and computerchoice == 1:
+  print("You win.")
+elif userchoice == 1 and computerchoice == 0:
+  print("You win.")
+elif userchoice == 0 and computerchoice == 2:
+  print("You win.")
